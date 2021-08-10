326c0e072f434e6650285e0293b20a63657342b9
TuziMoe <a@tuzi.moe>
2021-08-11 03:05:27 +1200

    init: yumeket

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,48 @@
 <template>
-  <hello-world />
+  <v-app id="inspire">
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <template v-for="n in 4">
+            <v-col
+              :key="n"
+              class="mt-2"
+              cols="12"
+            >
+              <strong>Category {{ n }}</strong>
+            </v-col>
+
+            <v-col
+              v-for="j in 6"
+              :key="`${n}${j}`"
+              cols="6"
+              md="2"
+            >
+              <v-sheet height="150"></v-sheet>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
-export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app id="inspire">
+    <v-main class="white lighten-3">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
+              <div class="text-center">
+                <v-col class="pt-15" cols="12">
+                  <v-img
+                    :src="require('../assets/yumeket.svg')"
+                    class="my-3"
+                    contain
+                    height="70"
+                  />
+                </v-col>
+                <v-col class="mb-4">
+                  <p class="subheading font-weight-regular">
+                    Spice up your creating process
+                  </p>
+                </v-col>
+                <v-col class="mb-1">
+                  <v-btn rounded class="create" style="width: 15rem">
+                    Create an account
+                  </v-btn>
+                </v-col>
+                <v-col class="mb-4">
+                  <v-btn rounded class="login" style="width: 15rem">
+                    Login
+                  </v-btn>
+                </v-col>
+                <v-col class="mt-4"> Begin with an existing account </v-col>
+                <v-col class="mt-4">
+                  <v-btn icon>
+                    <v-img
+                    :src="require('../assets/metamask.svg')"
+                    class="my-3"
+                    contain
+                    height="70"
+                  />
+                  </v-btn>
+                </v-col>
+              </div>
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.create {
+  background: linear-gradient(90deg, #72edf2 -20.33%, #5151e5 119.33%);
+  color: white;
+  text-transform: none !important;
+}
+.login {
+  background-color: rgba(0, 0, 0, 0.04) !important;
+  text-transform: none !important;
+}
+</style>
