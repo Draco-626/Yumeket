6397706cf7eb014736ace6acae597f54e061cd1b
Draco-626 <dracolongzhang@gmail.com>
2021-08-10 18:43:03 +0800

    added

---
+++ yumeket/my-app/src/pages/Page2.js
@@ -0,0 +1,79 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+import styles from './Dashboard.module.css';
+
+const Frame = (
+) => {
+    return (<div className={styles.frame}><p className={styles.dashboard}>Dashboard</p>
+        <div className={styles.relativeWrapperTwo}><div className={styles.flexWrapperFour}>
+            <p className={styles.home}>Home</p></div><div className={styles.flexWrapperFive}>
+                <p className={styles.works}>Works</p></div></div><div className={styles.flexWrapperSix}>
+            <div className={styles.flexWrapperSeven}><div className={styles.relativeWrapperOne}>
+                <p className={styles.recentlyUploadedWorks}>Recently uploaded works</p>
+                <img alt="" className={styles.i1}
+                    src="https://static.overlay-tech.com/assets/e8207556-b5c3-47c6-9939-6ec10142d89a.png" />
+            </div><p className={styles.viewAll}>View all</p></div><img alt="" className={styles.dash51}
+                src="https://static.overlay-tech.com/assets/2549dc18-fc30-44fa-bfbd-7ce2397ab961.png" /><
+                    p className={styles.tryPostingYourWork}>Try posting your work</p><div className={styles.flexWrapperEight}>
+                <p className={styles.postYourWork}>Post your work</p></div></div><div className={styles.flexWrapperTwenty}>
+            <div className={styles.flexWrapperNine}><div className={styles.flexWrapperTen}>
+                <p className={styles.totalReactions}>Total reactions</p><img alt="" className={styles.i2}
+                    src="https://static.overlay-tech.com/assets/27589f3b-fe57-4d0f-874b-1ca04ab7fded.png" />
+            </div><div className={styles.flexWrapperTwentyTwo}><img alt="" className={styles.dash41}
+                src="https://static.overlay-tech.com/assets/c4323389-0a42-4ab0-be36-c2cb4e3e9e57.png" />
+                    <p className={styles.views}>Views</p></div><p className={styles.one}>0</p>
+                <div className={styles.flexWrapperTwentyThree}><img alt="" className={styles.dash31}
+                    src="https://static.overlay-tech.com/assets/5485e235-322c-4568-9d80-4a8799d75104.png"
+                /><p className={styles.likes}>Likes</p></div><p className={styles.like}>0</p><div className={styles.flexWrapperTwentyFour}><img
+                    alt="" className={styles.dash21} src="https://static.overlay-tech.com/assets/68ed0d45-07af-482a-808d-2bd2c43d4f60.png" />
+                    <p className={styles.bookmarks}>Bookmarks</p></div><p className={styles.bookmark}>0</p><div className={styles.flexWrapperTwentyFour}>
+                    <img alt="" className={styles.dash11} src="https://static.overlay-tech.com/assets/256544eb-767d-4ad5-a504-ad755642bd09.png" />
+                    <p className={styles.comments}>Comments</p></div><p className={styles.comment}>0</p><div className={styles.flexWrapperEleven}>
+                    <p className={styles.viewDetails}>View details</p></div><p className={styles.latestUpdateAugust12021200}>
+                    Latest update: August 1, 2021 2:00</p></div><div className={styles.flexWrapperTwelve}><div
+                        className={styles.flexWrapperThirteen}><p className={styles.yumeketTodaysTheme}>Yumeket Today&#039;s Theme</p>
+                        <img alt="" className={styles.i1Two}
+                            src="https://static.overlay-tech.com/assets/36f9b167-ed00-4f6b-93f2-909531560043.png" />
+                    </div><div className={styles.flexWrapperFourteen}><p className={styles.foldingFan}>folding fan</p>
+                    <p className={styles.shanzi}>#扇子</p><p className={styles.letsDraw}>Let&#039;s draw!</p>
+                </div><div className={styles.flexWrapperTwentySix}><p className={styles.onceYouveCompletedTheWorkAddThe}>
+                    Once you&#039;ve completed the work, add the</p>
+                    <p className={styles.tagAndPostIt}>tag and post it!</p></div>
+                <div className={styles.flexWrapperFifteen}>
+                    <p className={styles.postYourWork}>Post your work</p>
+                </div></div><div className={styles.flexWrapperOne}>
+                <div className={styles.flexWrapperTwo}>
+                    <p className={styles.ongoingContests}>Ongoing contests</p>
+                </div><img alt="" className={styles.rectangle}
+                    src="https://static.overlay-tech.com/assets/3a147f89-5bd0-4ef3-af0a-c3533c4d9779.png" />
+                <p className={styles.num3}>第3回キネティックノベル大賞 恋愛小説部門</p>
+                <div className={styles.flexWrapperTwentyFour}><div className={styles.flexWrapperThree}>
+                    <p className={styles.untilAug31st}>Until Aug 31st</p></div><p className={styles.num202Works}>202 works</p>
+                </div></div></div><div className={styles.flexWrapperSixteen}><div className={styles.rectangleTwo} />
+           
+            <p className={styles.pr}>PR</p></div><div className={styles.flexWrapperSeventeen}>
+            <img alt="" className={styles.vectorLeft1}
+                src="https://static.overlay-tech.com/assets/52920a0d-cde2-47a1-aecc-d961e1b95d17.png" />
+             <div>
+        <Link to="/" >
+            <img alt="" className={styles.logo}
+                src="https://static.overlay-tech.com/assets/a47512bd-8fee-4553-b657-30b86052c109.svg" />
+            </Link></div>
+            <div className={styles.flexWrapperEighteen}>
+                <img alt="" className={styles.vectorSearch1}
+                    src="https://static.overlay-tech.com/assets/504d31c9-a2b0-4162-952e-cada71aa0111.png" />
+                <p className={styles.searchWorks}>Search works</p></div><div className={styles.flexWrapperNineteen}>
+                <p className={styles.postYourWorkTwo}>Post your work</p><img alt="" className={styles.vectorSearch21}
+                    src="https://static.overlay-tech.com/assets/b1186b07-e9c6-400f-b2f9-3a96598bc76c.png" />
+            </div><img alt="" className={styles.vectorMail1}
+                src="https://static.overlay-tech.com/assets/a8f1c282-a4a0-45cb-b2e3-a9db41dfb480.png" />
+            <img alt="" className={styles.vectorRing1}
+                src="https://static.overlay-tech.com/assets/cac42632-a6cd-4bf8-af66-16e0683c583f.png" />
+            <img alt="" className={styles.rectangleThree}
+                src="https://static.overlay-tech.com/assets/6fa4ebb0-800c-4b28-b308-dcfc44e3b600.png" />
+            <img alt=""
+                className={styles.vector} src="https://static.overlay-tech.com/assets/d407a356-41f3-4cc6-817a-d9a5d31b5d8e.png" /></div>
+    </div>);
+};
+
+export default Frame;
